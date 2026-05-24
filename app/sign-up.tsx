78eec3cc6493d8cd6b6a45c447ad381d3a685f4d
Emanuel de Brito <emanuelbrit16@gmail.com>
2026-05-23 23:42:13 -0300

    Installing React Hook Form and Implementing it on Sign Up Form Component integrated with Zod Schema

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -2,10 +2,13 @@ import { Container } from "@/src/ui/components/container";
 import { Header } from "@/src/ui/containers/header";
 import { Logo } from "@/src/ui/containers/logo";
 import { SignUpForm } from "@/src/ui/containers/SignUp/sign-up-form";
+import { SignUpSchemaType } from "@/src/ui/containers/SignUp/signUpSchema";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignUpScreen = () => {
-  const handleSignUp = () => {};
+  const handleSignUp = (data: SignUpSchemaType) => {
+    console.log(data);
+  };
 
   return (
     <Container>
--- src/ui/containers/SignUp/sign-up-form.tsx
+++ src/ui/containers/SignUp/sign-up-form.tsx
@@ -1,14 +1,80 @@
+import {
+  signUpSchema,
+  SignUpSchemaType,
+} from "@/src/ui/containers/SignUp/signUpSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 import { Box } from "../../components/box";
 import { Button } from "../../components/button";
+import { TextInput } from "../../components/text-input";
 
 type SignUpFormProps = {
-  onSubmit: () => void;
+  onSubmit: (data: SignUpSchemaType) => void;
 };
 
 export const SignUpForm = ({ onSubmit }: SignUpFormProps) => {
+  const { control, handleSubmit } = useForm<SignUpSchemaType>({
+    resolver: zodResolver(signUpSchema),
+  });
+
   return (
     <Box>
-      <Button title="Criar conta" onPress={onSubmit} />
+      <Controller
+        control={control}
+        name="fullName"
+        render={({ field, fieldState }) => (
+          <TextInput
+            label="Nome Completo"
+            placeholder="Digite seu nome"
+            errorMessage={fieldState.error?.message}
+            value={field.value}
+            onChangeText={field.onChange}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="email"
+        render={({ field, fieldState }) => (
+          <TextInput
+            label="E-mail"
+            placeholder="Digite seu email"
+            keyboardType="email-address"
+            errorMessage={fieldState.error?.message}
+            value={field.value}
+            onChangeText={field.onChange}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="password"
+        render={({ field, fieldState }) => (
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            secureTextEntry
+            errorMessage={fieldState.error?.message}
+            value={field.value}
+            onChangeText={field.onChange}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="confirmPassword"
+        render={({ field, fieldState }) => (
+          <TextInput
+            label="Confirmar Senha"
+            placeholder="Confirme sua senha"
+            secureTextEntry
+            errorMessage={fieldState.error?.message}
+            value={field.value}
+            onChangeText={field.onChange}
+          />
+        )}
+      />
+      <Button mt="s16" title="Criar conta" onPress={handleSubmit(onSubmit)} />
     </Box>
   );
 };
