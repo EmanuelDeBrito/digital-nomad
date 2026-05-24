8924f72cfd01e4c8e9dd2e40a8690a3c66a2937d
Emanuel de Brito <emanuelbrit16@gmail.com>
2026-05-23 23:14:00 -0300

    Installing Zod and creating SignUp Schema with Zod

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/src/ui/components/button";
 import { Container } from "@/src/ui/components/container";
 import { Header } from "@/src/ui/containers/header";
 import { Logo } from "@/src/ui/containers/logo";
+import { SignUpForm } from "@/src/ui/containers/SignUp/sign-up-form";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignUpScreen = () => {
@@ -11,7 +11,7 @@ const SignUpScreen = () => {
     <Container>
       <SafeAreaView>
         <Header title="Criar Conta" />
-        <Button title="Criar conta" onPress={handleSignUp} />
+        <SignUpForm onSubmit={handleSignUp} />
         <Logo />
       </SafeAreaView>
     </Container>
---
+++ src/ui/containers/SignUp/signUpSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const SignUpSchema = z
+  .object({
+    fullName: z
+      .string({ error: "Campo obrigatório!" })
+      .min(7, "Nome muito curto"),
+    email: z
+      .string({ error: "Campo obrigatório!" })
+      .min(1, "O e-mail é obrigatório")
+      .pipe(z.email({ error: "E-mail inválido" })),
+    password: z
+      .string({ error: "Campo obrigatório!" })
+      .min(6, "A senha deve ter no mínimo 6 caracteres"),
+    confirmPassword: z
+      .string({ error: "Campo obrigatório!" })
+      .min(6, "A senha deve ter no mínimo 6 caracteres"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    error: "As senhas devem ser iguais",
+    path: ["confirmPassword"],
+  });
