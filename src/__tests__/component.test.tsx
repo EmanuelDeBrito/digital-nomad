99d11410c19e5300dde3f36804184f9456c41440
Emanuel de Brito <emanuelbrit16@gmail.com>
2026-05-25 09:41:06 -0300

    Using fireEvent function for simulate press action in a Pressable component

--- src/__tests__/component.test.tsx
+++ src/__tests__/component.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react-native";
+import { fireEvent, render, screen } from "@testing-library/react-native";
+import { useState } from "react";
 import { Pressable, Text, View } from "react-native";
 
 type ComponentProps = {
@@ -7,6 +8,8 @@ type ComponentProps = {
 };
 
 const Component = ({ label, isLoading }: ComponentProps) => {
+  const [count, setCount] = useState(0);
+
   if (isLoading) {
     return <Text>Is loading...</Text>;
   }
@@ -15,6 +18,14 @@ const Component = ({ label, isLoading }: ComponentProps) => {
     <View>
       <Pressable>
         <Text>{label}</Text>
+      </Pressable>
+
+      <Pressable
+        testID="count-area"
+        onPress={() => setCount((prev) => prev + 1)}
+      >
+        <Text>Count: {count}</Text>
+        <Text onPress={() => setCount(0)}>Reset Count</Text>
       </Pressable>
     </View>
   );
@@ -35,5 +46,17 @@ describe("Component", () => {
     const element = screen.getByText(/Is loading/);
 
     expect(element).toBeOnTheScreen();
+  });
+
+  it("Should show a correct count value", () => {
+    render(<Component label="foo bar" isLoading={false} />);
+
+    const element = screen.getByText(/Count: 0/);
+
+    expect(element).toBeOnTheScreen();
+
+    fireEvent.press(screen.getByTestId("count-area"));
+
+    expect(screen.getByText(/Count: 1/)).toBeOnTheScreen();
   });
 });
