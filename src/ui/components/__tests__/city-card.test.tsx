fe479fbb780ed9e0423c94bac25e2c167982b52a
Emanuel de Brito <emanuelbrit16@gmail.com>
2026-06-07 22:05:36 -0300

    Creating my first Snapshot Test

--- src/ui/components/__tests__/city-card.test.tsx
+++ src/ui/components/__tests__/city-card.test.tsx
@@ -3,6 +3,21 @@ import { screen } from "@testing-library/react-native";
 import { CityCard } from "../city-card";
 
 describe("<CityCard />", () => {
+  // Teste de Snapshot - Para atualizar o Snapshot você deve rodar o seguinte comando (--u ou --updateSnapshot)
+  it("Should render the component", () => {
+    RenderComponent(
+      <CityCard
+        cityPreview={{
+          id: "1",
+          name: "Rio de Janeiro",
+          country: "Brasil",
+          coverImage: "rio-de-janeiro.png",
+        }}
+      />,
+    );
+
+    expect(screen.toJSON()).toMatchSnapshot();
+  });
   it("Should show the favorite icon", () => {
     RenderComponent(
       <CityCard
